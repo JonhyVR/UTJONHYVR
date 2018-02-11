c58b33dac14687d4d0dcc1e8301be651e43f4541
JonathanVR <jvergara0796@hotmail.com>
2018-02-10 19:35:09 -0600

    Diseño
    Modificación pequeña a Main Activity

--- app/src/main/java/jvr/utcuates/DriverLoginActivity.kt
+++ app/src/main/java/jvr/utcuates/DriverLoginActivity.kt
@@ -1,19 +1,12 @@
 package jvr.utcuates
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 
@@ -48,17 +41,17 @@ class DriverLoginActivity : AppCompatActivity() {
         mRegistration = findViewById(R.id.registration) as Button
 
         mRegistration!!.setOnClickListener {
-            val email = mEmail!!.text.toString()
-            val password = mPassword!!.text.toString()
-            mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this@DriverLoginActivity) { task ->
-                if (!task.isSuccessful) {
-                    Toast.makeText(this@DriverLoginActivity, "Error", Toast.LENGTH_SHORT).show()
-                } else {
-                    val user_id = mAuth!!.currentUser!!.uid
-                    val current_user_db = FirebaseDatabase.getInstance().reference.child("Users").child("Drivers").child(user_id)
-                    current_user_db.setValue(true)
+                val email = mEmail!!.text.toString()
+                val password = mPassword!!.text.toString()
+                mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this@DriverLoginActivity) { task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this@DriverLoginActivity, "Error", Toast.LENGTH_SHORT).show()
+                    } else {
+                        val user_id = mAuth!!.currentUser!!.uid
+                        val current_user_db = FirebaseDatabase.getInstance().reference.child("Users").child("Drivers").child(user_id)
+                        current_user_db.setValue(true)
+                    }
                 }
-            }
         }
 
         mLogin!!.setOnClickListener {
