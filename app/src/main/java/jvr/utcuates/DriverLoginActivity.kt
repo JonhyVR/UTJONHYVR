9acd36d5a77e3a6b33f50a10e42d43732ea92daf
oscar hernandez hernandez <joner14@hotmail.com>
2018-02-12 19:45:28 -0600

    Revert "Merge branch 'Ruta' of https://github.com/JonhyVR/UTJONHYVR into Ruta"
    This reverts commit da68099dbb4af7ac7965b33fa7db62c350c85aae.

--- app/src/main/java/jvr/utcuates/DriverLoginActivity.kt
+++ app/src/main/java/jvr/utcuates/DriverLoginActivity.kt
@@ -1,21 +1,27 @@
 package jvr.utcuates
 
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import android.util.Log
 import android.view.View
-=======
-import android.support.v7.app.AppCompatActivity
->>>>>>> 0320d6eb7fc263ba7d23c011a9120b0d9577013c
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import jvr.utcuates.Base.BaseLogin
 
 
-class DriverLoginActivity : AppCompatActivity() {
+class DriverLoginActivity : AppCompatActivity(),BaseLogin {
+
+
     private var mEmail: EditText? = null
     private var mPassword: EditText? = null
     private var mLogin: Button? = null
@@ -50,7 +56,6 @@ class DriverLoginActivity : AppCompatActivity() {
         mLogin = findViewById(R.id.login) as Button
         mRegistration = findViewById(R.id.registration) as Button
 
-<<<<<<< HEAD
         registrar()
         login()
 
@@ -82,22 +87,13 @@ class DriverLoginActivity : AppCompatActivity() {
                     val user_id = mAuth!!.currentUser!!.uid
                     val current_user_db = FirebaseDatabase.getInstance().reference.child("Users").child("Drivers").child(user_id)
                     current_user_db.setValue(true)
-=======
-        mRegistration!!.setOnClickListener {
-                val email = mEmail!!.text.toString()
-                val password = mPassword!!.text.toString()
-                mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this@DriverLoginActivity) { task ->
-                    if (!task.isSuccessful) {
-                        Toast.makeText(this@DriverLoginActivity, "Error", Toast.LENGTH_SHORT).show()
-                    } else {
-                        val user_id = mAuth!!.currentUser!!.uid
-                        val current_user_db = FirebaseDatabase.getInstance().reference.child("Users").child("Drivers").child(user_id)
-                        current_user_db.setValue(true)
-                    }
->>>>>>> 0320d6eb7fc263ba7d23c011a9120b0d9577013c
                 }
+            }
         }
 
+    }
+
+    override fun login() {
         mLogin!!.setOnClickListener {
             val email = mEmail!!.text.toString() +  "@utmetropolitana.edu.mx"
             val password = mPassword!!.text.toString()
@@ -110,16 +106,4 @@ class DriverLoginActivity : AppCompatActivity() {
     }
 
 
-<<<<<<< HEAD
-=======
-    override fun onStart() {
-        super.onStart()
-        mAuth!!.addAuthStateListener(firebaseAuthListener!!)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mAuth!!.removeAuthStateListener(firebaseAuthListener!!)
-    }
->>>>>>> 0320d6eb7fc263ba7d23c011a9120b0d9577013c
 }
